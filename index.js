8c675e3c23aad0ce1f15029151dd94ec004308c4
AJ Guiles <ashiguiles@gmail.com>
2020-02-13 23:47:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Add your functions here
-function  map(sourceArray,callBack) {
-  newArr = [] 
-  for ( let i = 0; i < sourceArray.length; i++) {
-    let inst = sourceArray[i]
-    newArr.push(callBack(inst))   
-  }
- return newArr
-}
+// function  map(sourceArray,callBack) {
+//   newArr = [] 
+//   for ( let i = 0; i < sourceArray.length; i++) {
+//     let inst = sourceArray[i]
+//     newArr.push(callBack(inst))   
+//   }
+// return newArr
+// }
 
-function usingCb(map){
- map()
-}
+// function usingCb(map){
+// map()
+// }
