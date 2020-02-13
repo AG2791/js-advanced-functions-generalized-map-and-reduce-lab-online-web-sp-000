552d565f3447878b7d2f1bf20c88347dd199f783
AJ Guiles <ashiguiles@gmail.com>
2020-02-13 23:47:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,14 @@
 // function usingCb(map){
 // map()
 // }
+
+function map(src, cb) {
+  let r = []
+
+  for (let i = 0; i < src.length; i++) {
+    let theElement = src[i]
+    r.push(cb(theElement))
+  }
+
+  return r;
+}
